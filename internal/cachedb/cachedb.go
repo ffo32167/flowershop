d44938104ba8fdb20cf76c945e954cdd75a2e718
ffo32167 <dmi32167@yandex.ru>
2022-07-07 02:15:17 +0300

    sale added

--- internal/cachedb/cachedb.go
+++ internal/cachedb/cachedb.go
@@ -18,18 +18,18 @@ type SqlDB interface {
 	List() ([]internal.Product, error)
 	Sale(id, cnt int) (res int, err error)
 }
+
 type NoSqlDB interface {
-	ListCreate(ctx context.Context, products []internal.Product) error
-	List(ctx context.Context) ([]internal.Product, error)
+	ListCreate(products []internal.Product) error
+	List() ([]internal.Product, error)
 	Sale(id, cnt int) (int, error)
 }
 
 func New(ctx context.Context, sqlDB SqlDB, noSqlDB NoSqlDB) (CacheProducts, error) {
-
 	c := CacheProducts{sqlDB: sqlDB, noSqlDB: noSqlDB, ctx: ctx}
 	err := c.RenewCache()
 	if err != nil {
-		return CacheProducts{}, err
+		return CacheProducts{}, fmt.Errorf("cant renew cache: %w", err)
 	}
 	return c, nil
 }
@@ -37,9 +37,9 @@ func New(ctx context.Context, sqlDB SqlDB, noSqlDB NoSqlDB) (CacheProducts, erro
 func (c CacheProducts) RenewCache() error {
 	products, err := c.sqlDB.List()
 	if err != nil {
-		return err
+		return fmt.Errorf("cant get product list: %w", err)
 	}
-	err = c.noSqlDB.ListCreate(c.ctx, products)
+	err = c.noSqlDB.ListCreate(products)
 	if err != nil {
 		return fmt.Errorf("cant create redis list: %w", err)
 	}
@@ -47,9 +47,17 @@ func (c CacheProducts) RenewCache() error {
 }
 
 func (c CacheProducts) List() ([]internal.Product, error) {
-	return c.noSqlDB.List(c.ctx)
+	return c.noSqlDB.List()
 }
 
-func (c CacheProducts) Sale(int, int) (int, error) {
+func (c CacheProducts) Sale(id int, cnt int) (int, error) {
+	_, err := c.sqlDB.Sale(id, cnt)
+	if err != nil {
+		return 0, fmt.Errorf("cant sale in sql: %w", err)
+	}
+	_, err = c.noSqlDB.Sale(id, cnt)
+	if err != nil {
+		return 0, fmt.Errorf("cant sale in nosql: %w", err)
+	}
 	return 0, nil
 }
